5853c86e1498f7d122dcc59dbca6ff437a1704b7
kamrulhasan777 <kamrul.hassan1021@gmail.com>
2018-02-20 22:21:48 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+$cmb_demo->add_field( array(
+		'name' => esc_html__( 'Test Text Small', 'cmb2' ),
+		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
+		'id'   => $prefix . 'textsmall',
+		'type' => 'text_small',
+		// 'repeatable' => true,
+		// 'column' => array(
+		// 	'name'     => esc_html__( 'Column Title', 'cmb2' ), // Set the admin column title
+		// 	'position' => 2, // Set as the second column.
+		// );
+		// 'display_cb' => 'yourprefix_display_text_small_column', // Output the display of the column values through a callback.
+	) );
